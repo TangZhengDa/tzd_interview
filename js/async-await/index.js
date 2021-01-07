965d11af98a3888e4836a833aa1cc30cb691b760
TangZhengDa <695698426@qq.com>
2021-01-07 23:07:40 +0800

    2021-1-7

---
+++ js/async-await/index.js
@@ -0,0 +1,14 @@
+// async/await的原理是什么？把下面这段代码翻译成promise
+function getJson() {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      console.log(2);
+      resolve(2)
+    }, 2000)
+  })
+}
+async function testAsync() {
+  await getJson()
+  console.log(3);
+}
+testAsync()
