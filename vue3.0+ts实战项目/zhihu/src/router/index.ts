5749f27bac4dce809a454ef743b724001103237a
TangZhengDa <695698426@qq.com>
2021-03-04 23:09:41 +0800

    2021-3-4

--- vue3.0+ts实战项目/zhihu/src/router/index.ts
+++ vue3.0+ts实战项目/zhihu/src/router/index.ts
@@ -1,24 +1,26 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-
-const routes: Array<RouteRecordRaw> = [
+const routerHistory = createWebHistory()
+const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/Register.vue')
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: routerHistory,
   routes
 })
 
---
+++ vue3.0+ts实战项目/zhihu/src/views/Register.vue
@@ -0,0 +1,89 @@
+<template>
+  <a-form :label-col="labelCol" :wrapper-col="wrapperCol">
+    <a-form-item label="邮箱地址" v-bind="validateInfos.email">
+      <a-input v-model:value="modelRef.email" />
+    </a-form-item>
+    <a-form-item label="昵称" v-bind="validateInfos.nickName">
+      <a-input v-model:value="modelRef.nickName" />
+    </a-form-item>
+    <a-form-item label="密码" v-bind="validateInfos.password">
+      <a-input v-model:value="modelRef.password" />
+    </a-form-item>
+    <a-form-item label="重复密码" v-bind="validateInfos.Rpassword">
+      <a-input v-model:value="modelRef.Rpassword" />
+    </a-form-item>
+    <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
+      <a-button type="primary" @click.prevent="onSubmit">注册</a-button>
+      <a-button style="margin-left: 10px" @click="toLogin">已有账户？去登录</a-button>
+    </a-form-item>
+  </a-form>
+</template>
+<script lang="ts">
+import { defineComponent, reactive, toRaw } from 'vue'
+import { useForm } from '@ant-design-vue/use'
+import { useRouter } from 'vue-router'
+export default defineComponent({
+  setup () {
+    const router = useRouter()
+    const modelRef = reactive({
+      email: '',
+      nickName: '',
+      password: '',
+      Rpassword: ''
+    })
+    const { validate, validateInfos } = useForm(
+      modelRef,
+      reactive({
+        email: [
+          {
+            required: true,
+            message: '请输入邮箱地址'
+          }
+        ],
+        nickName: [
+          {
+            required: true,
+            message: '请输入昵称'
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: '请输入密码'
+          }
+        ],
+        Rpassword: [
+          {
+            required: true,
+            message: '请再次输入密码'
+          }
+        ]
+      })
+    )
+    // 注册
+    const onSubmit = () => {
+      validate()
+        .then(res => {
+          // console.log(res, toRaw(modelRef))
+          if (res !== 'error') {
+            console.log(123)
+          }
+        })
+        .catch(err => {
+          console.log('error', err)
+        })
+    }
+    const toLogin = () => {
+      router.push('/login')
+    }
+    return {
+      labelCol: { span: 4 },
+      wrapperCol: { span: 14 },
+      validateInfos,
+      toLogin,
+      modelRef,
+      onSubmit
+    }
+  }
+})
+</script>
